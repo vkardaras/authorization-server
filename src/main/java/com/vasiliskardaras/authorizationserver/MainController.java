14f8590d0bcd97b06056298980113d36635198a1
vkardaras <vasiliskardaras@gmail.com>
2025-06-25 11:20:13 +0300

    Get Principal
    Add endpoint to get user principal after social login

--- src/main/java/com/vasiliskardaras/authorizationserver/MainController.java
+++ src/main/java/com/vasiliskardaras/authorizationserver/MainController.java
@@ -1,13 +1,20 @@
 package com.vasiliskardaras.authorizationserver;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class MainController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World";
+        return "Welcome!";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal user) {
+        return user;
     }
 }
